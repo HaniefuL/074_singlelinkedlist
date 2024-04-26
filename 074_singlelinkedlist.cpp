c96fbae3614b6c08a6dfd0b271587d696179f97b
HaniefuL <haniefulgeofarry@gmail.com>
2024-04-26 14:24:29 +0700

    menuliskan bool serachNode

--- 074_singlelinkedlist.cpp
+++ 074_singlelinkedlist.cpp
@@ -10,5 +10,46 @@ struct Node {
 Node* START = NULL;
 
 void addNode() {
+	int nim;
+	string name;
+	Node* nodebaru = new Node();
+	cout << "Masukan NIM ";
+	cin >> nim;
+	cout << "Masukan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START !== NULL && nim <= START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+
+	Node* previos = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
+
+	nodeBaru->next = current;
+	previous->next = nodeBaru
+}
+
+bool serachNode(int nim, Node* current, Node previous) {
 
 }
